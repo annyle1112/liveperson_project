3339e4652b6d8fe2a89edc9573c36217af927afa
amelieender <amelie.ender@gmx.de>
2021-10-29 11:22:04 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+import requests
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+if BEARER_TOKEN is None:
+    exit()
+
+
+def search_twitter(query, tweet_fields, bearer_token=BEARER_TOKEN):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
+        query, tweet_fields
+    )
+    response = requests.request("GET", url, headers=headers)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+# search term
+query = "skateboarding dog"
+
+# twitter fields to be returned by api call
+tweet_fields = "tweet.fields=text,author_id,created_at"
+
+# twitter api call
+json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
+
+# pretty printing
+print(json.dumps(json_response, indent=4, sort_keys=True))
